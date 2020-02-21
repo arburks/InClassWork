6bbceb58c08111697ee01c0465842b4eb5d73205
Austin Burks <arburks@ou.edu>
2020-02-20 20:16:31 -0600

    02.20.20

---
+++ 02.20.20 InClassWork/02.20.20 InClassWork/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._20._20_InClassWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] charArray = { 'a', 'b', 'c' };
+            char[] charArray2 = new char[3];
+            //^^three slots in this char slot: 0,1,2
+
+            int[] intArray = { 1, 2, 3 };
+            int[] intArray2 = new int[4];
+            intArray2[0] = 1;
+            intArray2[1] = 2;
+            intArray2[2] = 1;
+            intArray2[3] = 4;
+
+            string sentence = "The quick brown fox jumps over the lazy dog.";
+
+            char sentenceChar = sentence[2];
+
+            //foreach(char letter in sentence)
+            //{
+            //    Console.WriteLine($"{letter}");
+            //}
+
+            //string sentenceLength = sentence.PadLeft(10);
+            int sentenceLength = sentence.Length;
+            string truncateSentence = sentence.Substring(0, 19);
+            string lowerCaseSentence = sentence.ToLower();
+            string upperCaseSentence = sentence.ToUpper();
+            string trimmedSentence = sentence.Trim();
+
+            char[] trimChars = { 'x', 'y', 'z' };
+            string trimmedCharSentence = sentence.Trim(trimChars);
+            bool removeBadChars = sentence.Contains("fox");
+            string[] splitSentence = trimmedSentence.Split('-');
+
+            Console.WriteLine(splitSentence[0]);
+
+            Console.WriteLine(truncateSentence);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Participation 6.1 Enum.Struct/Participation 6.1 Enum.Struct/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Convert;
+
+namespace Participation_6._1_Enum.Struct
+{
+    //by Austin Burks
+    //Default enum orientation is Int32
+    enum Orientation
+    {
+        North = 1,
+        South = 2,
+        East = 3,
+        West = 4
+    }
+
+    struct Route
+    {
+        public Orientation direction;
+        public double distance;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Orientation myDirection = Orientation.North;
+            //Orientation antoherDirection = Orientation.South;
+            //Random randomNumber = new Random();
+
+            //WriteLine($"myDirection = {myDirection}");
+
+            Route myRoute;
+            int myDirection = -1;
+            double myDistance;
+            WriteLine("1) North\n2) South\n3) East\n4) West");
+
+            do
+            {
+                WriteLine("Select a Direction:");
+                myDirection = ToInt32(ReadLine());
+            } while ((myDirection < 1) || (myDirection > 4));
+
+            WriteLine("Input a distance:");
+            myDistance = ToDouble(ReadLine());
+            myRoute.direction = (Orientation)myDirection;
+            myRoute.distance = myDistance;
+            WriteLine($"myRoute specifies a direction of {myRoute.direction} and a distance of {myRoute.distance}");
+
+            WriteLine("");
+            WriteLine("Press any key to exit ... ");
+            ReadKey();
+
+        }
+    }
+}
