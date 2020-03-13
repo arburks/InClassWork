eefeb646623ed9c3cc73080f2193a052e33508a3
Austin Burks <arburks@ou.edu>
2020-03-12 19:44:44 -0500

    03.12.20

---
+++ 02.12.20 InClassWork/Participation 8.2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Participation_8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var favoriteThings = new List<string>();
+            string answer;
+            do
+            {
+                Console.WriteLine("Please enter one of your favorite things >>");
+                favoriteThings.Add(Console.ReadLine());
+
+                Console.WriteLine("Do you have another favorite thing to add? Y/N >>");
+                answer = Console.ReadLine().ToUpper()[0].ToString();
+            }
+            while (answer != "N");
+
+            Random random = new Random();
+            int randomNumber = random.Next(0, (favoriteThings.Count()-1));
+            Console.WriteLine($"Favorite thing { favoriteThings[randomNumber] }");
+
+            Console.ReadLine();
+
+            Dictionary<int, double> studentGrades = new Dictionary<int, double>();
+            studentGrades.Add(10, 3.8);
+            studentGrades.Add(11, 3.9);
+            studentGrades.Add(12, 3.4);
+
+            Console.WriteLine("Please pick a student ID to see their GPA: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            if(studentGrades.ContainsKey(userInput))
+            {
+                Console.WriteLine($"The student with ID {userInput} has a GPA of {studentGrades[userInput]}");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you entered an invalid ID. I award you no points and may God have mercy on your soul. Goodbye!!");
+            }
+ 
+        }
+    }
+}
---
+++ 02.12.20 InClassWork/Participation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Participation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> courseGrade = new Dictionary<string, List<double>>();
+            List<double> listofnum = new List<double>() { 98, 50, 60, 55, 89 };
+            List<double> listofnum1 = new List<double>() { 88, 70, 69, 96, 79 };
+            List<double> listofnum2 = new List<double>() { 99, 89, 88, 76, 91 };
+
+            courseGrade.Add("mis3013", new List<double>());
+            courseGrade["mis3013"].AddRange(listofnum);
+
+            courseGrade.Add("acct2013", new List<double>());
+            courseGrade["acct2013"].AddRange(listofnum1);
+
+            courseGrade.Add("econ2213", new List<double>());
+            courseGrade["econ2213"].AddRange(listofnum2);
+            courseGrade["econ2213"].Add(56);
+
+            foreach (var courseCode in courseGrade.Keys)
+            {
+                double sum = 0;
+
+                foreach (var grade in courseGrade[courseCode])
+                {
+                    sum += grade;
+                }
+                double sumAsPercent = sum / 100;
+                var count = sumAsPercent / courseGrade[courseCode].Count;
+                Console.WriteLine($"The average for {courseCode} is {count.ToString("P")}");
+                
+            }
+          
+            Console.ReadKey();
+        }   
+    }
+}
